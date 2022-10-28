cfae2fd7a6d9bba3798bec5bd4a7d513beabf888
Xiangyi Lin MacBook Pro VSCode <xil176@ucsd.edu>
2022-10-27 19:17:22 -0700

    update explore and readme

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # Lab 5 - Starter
 
 Name:
+
     * Xiangyi Lin
 
+
+Expose:
+
+    * https://alina10050805.github.io/Lab5_Starter/expose.html
+
+
 Explore:
+
+    * Pt 1. Speech Synthesis: https://alina10050805.github.io/Lab5_Starter/explore.html
+    
     * Pt 2. GitHub Actions: https://github.com/alina10050805/introduction-to-github
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -6,21 +6,23 @@ function init() {
   // TODO
 
   const synth = window.speechSynthesis;
-  const select = document.querySelector('select');
+  const select = document.getElementById('voice-select');
   const button = document.querySelector('button');
   const text = document.getElementById('text-to-speak');
   const smilingImg = document.querySelector('img');
  
   let voices = []
 
-  voices = synth.getVoices();
-  for(let i=0; i<voices.length; i++)
-  {
-    let option = document.createElement('option');
-    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
-    option.value = voices[i].name;
-    select.appendChild(option);
-  }
+  synth.addEventListener("voiceschanged", function() {
+    voices = synth.getVoices();
+    for(let i=0; i<voices.length; i++)
+    {
+      let option = document.createElement('option');
+      option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+      option.value = voices[i].name;
+      select.appendChild(option);
+    }
+  })
 
   button.addEventListener('click', function() {
     let utter = new SpeechSynthesisUtterance(text.value);
